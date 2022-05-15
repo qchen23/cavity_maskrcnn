648a49b8397c33180a9a03802874d959428f6d08
czheng4 <czheng4@vols.utk.edu>
2022-05-15 22:17:57 +0000

    ok

--- QC_bubble/bubble_detection.py
+++ QC_bubble/bubble_detection.py
@@ -77,6 +77,8 @@ def detect(model_path = "bubble_mask_rcnn.h5", images_path = [], nm_pixels = [],
     if img_max_dim >range_lst[-1]:
       new_size = math.ceil(img_max_dim/64.0)*64
       img_size_lst = [new_size + 512 * 2, new_size + 512 * 1, new_size, new_size - 512, new_size - 512 * 2]
+    else:
+      img_size_lst = [new_size] + img_size_lst
 
     for item in img_size_lst:
       info = ExtractMask(img_name, fitting_type, item)     
